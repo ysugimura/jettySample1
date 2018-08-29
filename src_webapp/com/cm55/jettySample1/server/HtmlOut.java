c96d53494184e7dbda6635cbb212e2a053d78879
ysugimura <ysugimura@cm55.com>
2018-08-29 19:57:09 +0900

    修正

---
+++ src_webapp/com/cm55/jettySample1/server/HtmlOut.java
@@ -0,0 +1,15 @@
+package com.cm55.jettySample1.server;
+
+import java.io.*;
+
+import javax.servlet.http.*;
+
+public class HtmlOut {
+
+  static void output(HttpServletResponse r, String html) throws IOException {
+    OutputStream out = r.getOutputStream();
+    out.write(html.getBytes("UTF-8"));
+    out.close();
+    r.setStatus(HttpServletResponse.SC_OK);
+  }
+}
--- src_webapp/com/cm55/jettySample1/server/Servlet1.java
+++ src_webapp/com/cm55/jettySample1/server/Servlet1.java
@@ -1,8 +1,15 @@
 package com.cm55.jettySample1.server;
 
+import java.io.*;
+
 import javax.servlet.http.*;
 
 public class Servlet1 extends HttpServlet {
 
-
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse r) throws IOException {
+    HtmlOut.output(r, 
+      "<html><body>test1</body></html>"
+    );
+  }
 }
